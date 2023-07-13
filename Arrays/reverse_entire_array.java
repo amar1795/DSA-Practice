908a959ceba87fd0b60610c843f1058bc19567a3
Amar <jeetamar496@gmail.com>
2023-07-13 15:22:24 +0530

    python file added

--- Arrays/reverse_entire_array.java
+++ Arrays/reverse_entire_array.java
@@ -1,16 +1,31 @@
 package Arrays;
 
 import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class reverse_entire_array {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int length = sc.nextInt();
+
+        int[] array = new int[length];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < length; i++) {
+            array[i] = sc.nextInt();
+        }
         System.out.println("Original array: " + Arrays.toString(array));
 
         reverseArray(array);
 //        reversing an array
 
         System.out.println("Reversed array: " + Arrays.toString(array));
+        sc.close();
+
     }
 
     public static void reverseArray(int[] array) {
