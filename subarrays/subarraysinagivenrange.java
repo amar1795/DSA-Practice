cb2a7d1a2adc873680ab3074282dbcd1029bd10f
Amar <jeetamar496@gmail.com>
2023-07-25 16:54:07 +0530

    subararys in a given range

---
+++ subarrays/subarraysinagivenrange.java
@@ -0,0 +1,19 @@
+package subarrays;
+
+public class subararysinagivenrange {
+    public int[] solve(int[] A, int B, int C) {
+
+
+        int ans[]=new int[C-B+1];
+        int j=0;
+        for(int i=B;i<=C;i++)
+        {
+            ans[j]=A[i];
+            j++;
+        }
+
+        return ans;
+
+
+    }
+}
