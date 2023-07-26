b3f7e9967786d5cbe8674cc8112698b38bb21592
Amar <jeetamar496@gmail.com>
2023-07-26 13:26:58 +0530

    rotating the 2d arrays 90 degree TC: 0(N^2) SC:0(1)

---
+++ multidimensionalarrays/rotate90degree.java
@@ -0,0 +1,36 @@
+package multidimensionalarrays;
+
+public class rotate90degree {
+
+
+        public void solve(int[][] A) {
+            int n=A.length;
+            int temp=0;
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<i;j++)
+                {
+                    // transposing the matrix
+                    temp=A[i][j];
+                    A[i][j]=A[j][i];
+                    A[j][i]=temp;
+
+                }
+            }
+
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n/2;j++)
+                {
+                    // swapping the matrix
+                    temp=A[i][j];
+                    A[i][j]=A[i][n-j-1];
+                    A[i][n-j-1]=temp;
+
+                }
+
+            }
+        }
+
+
+}
