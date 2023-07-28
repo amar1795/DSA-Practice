5df76e5ff0d63a971060c0e8755f353b7acc8512
Amar <jeetamar496@gmail.com>
2023-07-28 20:34:58 +0530

    transpose of a 2d matrix

---
+++ multidimensionalarrays/transpose.java
@@ -0,0 +1,23 @@
+package multidimensionalarrays;
+
+public class transpose {
+
+        public int[][] solve(int[][] A) {
+            int n=A.length;
+            int m=A[0].length;
+            int arr[][]=new int[m][n];
+            for(int i=0;i<n;i++)
+            {
+
+                for(int j=0;j<m;j++)
+                {
+                    arr[j][i]=A[i][j];
+
+                }
+
+            }
+            return arr;
+        }
+
+
+}
