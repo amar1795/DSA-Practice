f4478bc782f91154ac59eb74ccaea258c3fdc158
Amar <jeetamar496@gmail.com>
2023-07-29 23:44:51 +0530

    subarray with the given sum and length

---
+++ subarrays/subarraysum.java
@@ -0,0 +1,40 @@
+package subarrays;
+
+public class subarraysum {
+
+        public int solve(int[] A, int B, int C) {
+
+            int n=A.length;
+            int s=0;int k=B-1;
+            int sum=0;
+            while(s<=k)
+            {
+                sum+=A[s];
+                s++;
+            }
+
+            if(sum==C)
+            {
+                return 1;
+            }
+            s=1; k=B;
+
+            while(k<n)
+            {
+                sum=sum-A[s-1];
+                sum=sum+A[k];
+                s++;
+                k++;
+
+
+                if(sum==C)
+                {
+                    return 1;
+                }
+            }
+
+            return 0;
+        }
+
+
+}
